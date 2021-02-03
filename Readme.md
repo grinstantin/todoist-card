dc0ac2cc993c2abdb363cb9fc142749697a1ce06
Konstantin Grinkevich <34913257+grinstantin@users.noreply.github.com>
2021-02-03 20:10:10 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,95 @@
+# Todoist Card
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-orange.svg)](https://github.com/custom-components/hacs)
+
+Todoist card for [Home Assistant](https://www.home-assistant.io) Lovelace UI. This card displays items from selected Todoist project.
+
+![Preview of todoist-card](https://user-images.githubusercontent.com/34913257/106402171-f82f7680-6430-11eb-8dba-71f3b42712f9.png)
+
+## Installing
+
+### HACS
+
+This card is available as a [custom repository](https://hacs.xyz/docs/faq/custom_repositories) in [HACS](https://hacs.xyz) (Home Assistant Community Store).
+
+### Manual
+
+1. Download `todoist-card.js` file from the [latest-release](https://github.com/grinstantin/todoist-card/releases/latest).
+2. Put `todoist-card.js` file into your `config/www` folder.
+3. Add a reference to `todoist-card.js` in Lovelace. There's two way to do that:
+   1. **Using UI:** _Configuration_ → _Lovelace Dashboards_ → _Resources_ → Click Plus button → Set _Url_ as `/local/todoist-card.js` → Set _Resource type_ as `JavaScript Module`.
+   2. **Using YAML:** Add the following code to `lovelace` section.
+      ```yaml
+      resources:
+        - url: /local/todoist-card.js
+          type: module
+      ```
+4. Add `custom:todoist-card` to Lovelace UI as any other card (using either editor or YAML configuration).
+
+## Using the card
+
+This card can be configured using Lovelace UI editor.
+
+1. Add the following code to `configuration.yaml`:
+    ```yaml
+    sensor:
+      - platform: rest
+        name: To-do List
+        method: GET
+        resource: 'https://api.todoist.com/sync/v8/projects/get_data'
+        params:
+          token: !secret todoist_api_token
+          project_id: TODOIST_PROJECT_ID
+        value_template: '{{value_json[''project''][''id'']}}'
+        json_attributes:
+          - items
+        scan_interval: 30
+
+    rest_command:
+      todoist:
+        method: post
+        url: 'https://api.todoist.com/sync/v8/sync'
+        payload: !secret todoist_api_payload
+        content_type: 'application/x-www-form-urlencoded'
+    ```
+2. ... and to `secrets.yaml`:
+    ```yaml
+    todoist_api_token: TODOIST_API_TOKEN
+    todoist_api_payload: token=TODOIST_API_TOKEN&commands={{commands}}
+    ```
+3. Replace `TODOIST_API_TOKEN` with your [token](https://todoist.com/prefs/integrations) and `TODOIST_PROJECT_ID` with ID of your selected Todoist project.
+    > Your can get `TODOIST_PROJECT_ID` from project URL, which usually looks like this:
+    `https://todoist.com/app/project/TODOIST_PROJECT_ID`
+4. Reload configs or restart Home Assistant.
+5. In Lovelace UI, click 3 dots in top left corner.
+6. Click _Edit Dashboard_.
+7. Click _Add Card_ button in the bottom right corner to add a new card.
+8. Find _Custom: Todoist Card_ in the list.
+9. Choose `entity`.
+10. Now you should see the preview of the card!
+
+Typical example of using this card in YAML config would look like this:
+
+```yaml
+type: 'custom:todoist-card'
+entity: sensor.to_do_list
+show_header: true
+```
+
+Here is what every option means:
+
+| Name           |   Type    | Default      | Description                                                    |
+| -------------- | :-------: | :----------: | ---------------------------------------------------------------|
+| `type`         | `string`  | **required** | `custom:todoist-card`                                          |
+| `entity`       | `string`  | **required** | An entity_id within the `sensor` domain.                       |
+| `show_header`  | `boolean` | `true`       | Show friendly name of the selected `sensor` in the card header.|
+
+## Actions
+
+- _Circle_ marks selected task as completed.
+- _Trash bin_ deletes selected task.
+- _Input_ adds new item to the list after pressing `Enter`.
+
+## License
+
+MIT © [Konstantin Grinkevich](https://github.com/grinstantin)
---
+++ todoist-card.js
@@ -0,0 +1,338 @@
+import {LitElement, html, css} from 'https://unpkg.com/lit-element@2.4.0/lit-element.js?module';
+
+class TodoistCardEditor extends LitElement {
+    static get properties() {
+        return {
+            hass: Object,
+            _config: Object,
+        };
+    }
+    
+    get _entity() {
+        if (this._config) {
+            return this._config.entity || '';
+        }
+        
+        return '';
+    }
+    
+    get _show_header() {
+        if (this._config) {
+            return this._config.show_header || true;
+        }
+        
+        return true;
+    }
+    
+    setConfig(config) {
+        this._config = config;
+    }
+    
+    configChanged(config) {
+        const e = new Event('config-changed', {
+            bubbles: true,
+            composed: true,
+        });
+        
+        e.detail = {config: config};
+        
+        this.dispatchEvent(e);
+    }
+    
+    getEntitiesByType(type) {
+        return this.hass
+            ? Object.keys(this.hass.states).filter(entity => entity.substr(0, entity.indexOf('.')) === type)
+            : [];
+    }
+    
+    valueChanged(e) {
+        if (
+            !this._config
+            || !this.hass
+            || (this[`_${e.target.configValue}`] === e.target.value)
+        ) {
+            return;
+        }
+        
+        if (e.target.configValue) {
+            if (e.target.value === '') {
+                if (e.target.configValue !== 'entity') {
+                    delete this._config[e.target.configValue];
+                }
+            } else {
+                this._config = {
+                    ...this._config,
+                    [e.target.configValue]: e.target.checked !== undefined
+                        ? e.target.checked
+                        : e.target.value,
+                };
+            }
+        }
+        
+        this.configChanged(this._config);
+    }
+    
+    render() {
+        if (!this.hass) {
+            return html``;
+        }
+        
+        const entities = this.getEntitiesByType('sensor');
+
+        return html`<div class="card-config">
+            <paper-dropdown-menu
+                label="Entity (required)"
+                .configValue=${'entity'}
+                @value-changed=${this.valueChanged}
+            >
+                <paper-listbox
+                    slot="dropdown-content"
+                    .selected=${entities.indexOf(this._config.entity || '')}
+                >
+                    ${entities.map(entity => {
+                        return html`<paper-item>${entity}</paper-item>`;
+                    })}
+                </paper-listbox>
+            </paper-dropdown-menu>
+            
+            <p class="option">
+                <ha-switch
+                    .checked=${(this._config.show_header === undefined) || (this._config.show_header !== false)}
+                    .configValue=${'show_header'}
+                    @change=${this.valueChanged}
+                >
+                </ha-switch>
+                Show header
+            </p>
+        </div>`;
+    }
+    
+    static get styles() {
+        return css`
+            .card-config paper-dropdown-menu {
+                width: 100%;
+            }
+            
+            .option {
+                display: flex;
+                align-items: center;
+            }
+            
+            .option ha-switch {
+                margin-right: 10px;
+            }
+        `;
+    }
+}
+
+
+class TodoistCard extends LitElement {
+    static get properties() {
+        return {
+            hass: Object,
+            config: Object,
+        };
+    }
+    
+    static getConfigElement() {
+        return document.createElement('todoist-card-editor');
+    }
+
+    setConfig(config) {
+        if (!config.entity) {
+            throw new Error('Entity is not set!');
+        }
+        
+        this.config = config;
+    }
+
+    getCardSize() {
+        return this.hass.states[this.config.entity].attributes.items.length || 1;
+    }
+    
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    
+    itemAdd(e) {
+        if (e.which === 13) {
+            let input = this.shadowRoot.getElementById('todoist-card-item-add');
+            let value = input.value;
+            
+            if (value && value.length > 1) {
+                let stateValue = this.hass.states[this.config.entity].state || undefined;
+                
+                if (stateValue) {
+                    let date = new Date();
+                    
+                    let temp = this.random(1, 100) + '-' + (+date) + '-' + date.getMilliseconds();
+                    
+                    let commands = [{
+                        'type': 'item_add',
+                        'temp_id': temp,
+                        'uuid': temp,
+                        'args': {
+                            'project_id': stateValue,
+                            'content': value,
+                        },
+                    }];
+                    
+                    this.hass.callService('rest_command', 'todoist', {
+                        commands: JSON.stringify(commands),
+                    });
+                    
+                    input.value = '';
+                    
+                    let t = this;
+                    setTimeout(function () {
+                        t.hass.callService('homeassistant', 'update_entity', {
+                            entity_id: t.config.entity,
+                        });
+                    }, 1000);
+                }
+            }
+        }
+    }
+    
+    itemClose(itemId) {
+        let date = new Date();
+        
+        let commands = [{
+            'type': 'item_close',
+            'uuid': this.random(1, 100) + '-' + (+date) + '-' + date.getMilliseconds(),
+            'args': {
+                'id': itemId,
+            },
+        }];
+        
+        this.hass.callService('rest_command', 'todoist', {
+            commands: JSON.stringify(commands),
+        });
+        
+        let t = this;
+        setTimeout(function () {
+            t.hass.callService('homeassistant', 'update_entity', {
+                entity_id: t.config.entity,
+            });
+        }, 1000);
+    }
+    
+    itemDelete(itemId) {
+        let date = new Date();
+        
+        let commands = [{
+            'type': 'item_delete',
+            'uuid': this.random(1, 100) + '-' + (+date) + '-' + date.getMilliseconds(),
+            'args': {
+                'id': itemId,
+            },
+        }];
+        
+        this.hass.callService('rest_command', 'todoist', {
+            commands: JSON.stringify(commands),
+        });
+        
+        let t = this;
+        setTimeout(function () {
+            t.hass.callService('homeassistant', 'update_entity', {
+                entity_id: t.config.entity,
+            });
+        }, 1000);
+    }
+
+    render() {
+        let state = this.hass.states[this.config.entity] || undefined;
+        
+        if (!state) {
+            return html``;
+        }
+        
+        let items = state.attributes.items || [];
+        
+        return html`<ha-card>
+            ${(this.config.show_header === undefined) || (this.config.show_header !== false)
+                ? html`<h1 class="card-header">
+                    <div class="name">${state.attributes.friendly_name}</div>
+                </h1>`
+                : html``
+            }
+            ${items.length
+                ? html`<div class="todoist-list">
+                    ${items.map(item => {
+                        return html`<div class="todoist-item">
+                            <ha-icon-button
+                                icon="mdi:checkbox-blank-circle-outline"
+                                class="todoist-item-close"
+                                @click=${() => this.itemClose(item.id)}
+                            ></ha-icon-button>
+                            <span>${item.content}</span>
+                            <ha-icon-button
+                                icon="mdi:trash-can-outline"
+                                class="todoist-item-delete"
+                                @click=${() => this.itemDelete(item.id)}
+                            ></ha-icon-button>
+                        </div>`;
+                    })}
+                </ul>`
+                : html`<div class="todoist-list-empty">No uncompleted tasks!</div>`}
+            <input
+                id="todoist-card-item-add"
+                type="text"
+                class="todoist-item-add"
+                placeholder="New item..."
+                @keyup=${this.itemAdd}
+            />
+        </ha-card>`;
+    }
+    
+    static get styles() {
+        return css`
+            .card-header {
+                padding-bottom: unset;
+            }
+            
+            .todoist-list {
+                padding: 15px;
+            }
+            
+            .todoist-list-empty {
+                padding: 15px;
+                text-align: center;
+                font-size: 24px;
+            }
+            
+            .todoist-item {
+                line-height: 48px;
+            }
+            
+            .todoist-item > span {
+                font-size: 16px;
+            }
+            
+            .todoist-item-delete {
+                float: right;
+            }
+            
+            .todoist-item-add {
+                width: calc(100% - 30px);
+                height: 32px;
+                margin: 0 15px 15px;
+                padding: 10px;
+                box-sizing: border-box;
+                border-radius: 5px;
+                font-size: 16px;
+            }
+        `;
+    }
+}
+
+customElements.define('todoist-card-editor', TodoistCardEditor);
+customElements.define('todoist-card', TodoistCard);
+
+window.customCards = window.customCards || [];
+window.customCards.push({
+    preview: true,
+    type: 'todoist-card',
+    name: 'Todoist Card',
+    description: 'Custom card for displaying lists from Todoist.',
+});
